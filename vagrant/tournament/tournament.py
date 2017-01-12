502ba595039a49d400aee3088c7872c7a6d5a3a3
Joe <joezhow777@gamil.com>
2017-01-13 01:41:06 +0800

    1.Improve database scheme. 2.All tests passed. 3.passed PEP8 online.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,63 +1,72 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
 
 
-def connect():
+def connect(database_name="tournament"):
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except Exception as e:
+        raise e
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn = connect()
-    c = conn.cursor()
-    c.execute('''delete from standings;''')
-    c.execute('''delete from match;''')
-    conn.commit()
-    conn.close()
+    db, cursor = connect()
+    query = "TRUNCATE match CASCADE;"
+    cursor.execute(query)
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = connect()
-    c = conn.cursor()
-    c.execute('''delete from player;''')
-    conn.commit()
-    conn.close()
+    db, cursor = connect()
+    query = "TRUNCATE player CASCADE;"
+    cursor.execute(query)
+    db.commit()
+    db.close()
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = connect()
-    c = conn.cursor()
-    c.execute('''select count(*) as num from player''')
-    count = c.fetchone()[0]
-    conn.close()
+    db, cursor = connect()
+    query = "SELECT count(*) as num from player"
+    cursor.execute(query)
+    count = cursor.fetchone()[0]
+    db.close()
     return count
+
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-    
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute("insert into player (name) values (%s);", (name,))
-    conn.commit()
-    conn.close()
+    db, cursor = connect()
+    query = "INSERT INTO player (name) values (%s);"
+    param = (name,)
+    cursor.execute(query, param)
+
+    db.commit()
+    db.close()
+
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
-    
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+
+    The first entry in the list should be the player in first place,
+    or a player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -66,27 +75,35 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    conn = connect()
-    c = conn.cursor()
+    db, cursor = connect()
 
-    c.execute('''
-        SELECT player.id, name, COALESCE(wins, 0), COALESCE(matches, 0) FROM 
-        player LEFT JOIN
-        (SELECT id, count(*) AS matches, sum(score) AS wins FROM standings 
-        GROUP BY id) AS st
-        ON player.id = st.id
-        ''')
+    query = '''SELECT player.id, name, COALESCE(wins, 0) , COALESCE(matches, 0)
+                from player left join
+                ((SELECT id, count(*) as matches from player, match
+                where player.id = match.loser
+                OR player.id = match.winner
+                group by player.id) as tm
+                LEFT JOIN
+                (SELECT winner as wid, count(*) as wins from match
+                group by winner) as tw
+                ON tw.wid = tm.id) as twm
+                ON player.id = twm.id
+                ORDER BY wins DESC
+               '''
+    cursor.execute(query)
+    result = cursor.fetchall()
 
-    result = c.fetchall()
-    conn.close()
+    db.close()
     return result
+
 
 class match(object):
     """docstring for match"""
     def __init__(self, arg):
         super(match, self).__init__()
         self.arg = arg
-        
+
+
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
 
@@ -94,27 +111,22 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    db, cursor = connect()
+    query = "insert into match (winner, loser) values (%s, %s)"
+    param = (winner, loser)
+    cursor.execute(query, param)
+    db.commit()
+    db.close()
 
-    conn = connect()
-    c = conn.cursor()
 
-    c.execute("insert into match (id1, id2) values (%s, %s)", (winner, loser))
-    conn.commit()
-    c.execute("select match from match order by match DESC")
-    match = c.fetchone()[0]
-    c.execute("insert into standings (id, match, score) values (%s, %s, %s)", (winner, match, 1))
-    c.execute("insert into standings (id, match, score) values (%s, %s, %s)", (loser, match, 0))
-    conn.commit()
-    conn.close()
- 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -122,19 +134,19 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute('''SELECT player.id, name, COALESCE(wins, 0) as wins FROM 
-        player LEFT JOIN
-        (SELECT id, count(*) AS matches, sum(score) AS wins FROM standings 
-        GROUP BY id) AS st
-        ON player.id = st.id
-        ORDER BY wins DESC''')
-    result = c.fetchall()
-    conn.close()
+    db, cursor = connect()
+    cursor = db.cursor()
+    cursor.execute('''SELECT player.id, name, wins FROM player LEFT JOIN
+        (SELECT winner, count(*) as wins from match group by winner) as tw
+        ON player.id = tw.winner
+        ORDER BY wins
+        ''')
+    result = cursor.fetchall()
+    db.close()
     pairings = []
     for index in range(0, len(result), 2):
-        item = (result[index][0],result[index][1],result[index+1][0],result[index+1][1])
+        item = (result[index][0], result[index][1],
+                result[index+1][0], result[index+1][1])
         pairings.append(item)
 
     return pairings
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,10 +6,9 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-create database tournament;
-
-drop table match;
-drop table player;
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
 
 create table player (
 id SERIAL primary key,
@@ -17,13 +16,8 @@ name text
 );
 
 create table match (
-id1 integer references player(id),
-id2 integer references player(id),
+winner integer references player(id),
+loser integer references player(id),
 match SERIAL primary key
 );
 
-create table standings (
-id integer references player(id),
-match integer references match(match),
-score integer
-);
